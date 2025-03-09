6c4ea5e588d10c3a1742e3649668546c18516227
Angel75Git <aduboncenteno@usf.edu>
2025-03-09 11:48:30 -0400

    Task 2 - Support Tickets Dynamic Addition

---
+++ cc_14.js
@@ -0,0 +1,45 @@
+//Task 2
+//Getting parent div ticketContainer
+const dashBoard = document.getElementById("ticketContainer");
+
+//New Support Ticket
+function newTicket(name, issue, Plevel) {
+    const ticket = document.createElement("div");
+    //Setting Attributes to cards
+    ticket.setAttribute("class", "card-support")
+    ticket.setAttribute("id", name)
+
+    //Name of Customer
+    const customerName = document.createElement("h1");
+    customerName.textContent = name;
+    
+    //Their Issue Description
+    const customerIssue = document.createElement("p");
+    customerIssue.textContent = issue;
+
+    //Priority Level
+    const customerLevel = document.createElement("label")
+    customerLevel.textContent = Plevel;
+
+    //Appending all Attributes to ticket
+    ticket.appendChild(customerName);
+    ticket.appendChild(customerIssue);
+    ticket.appendChild(customerLevel);
+
+    //Adding a Reemove Button
+    const resolveBtn = document.createElement("button");
+    resolveBtn.textContent = "Resolve";
+
+    //setting button attributes
+    resolveBtn.setAttribute("class", "Remove-button")
+    resolveBtn.setAttribute("id", "Remove-ticket")
+    
+    ticket.appendChild(resolveBtn)
+    //Connecting the ticket to dashboard
+    dashBoard.appendChild(ticket)
+}
+
+//New entries - support Tickets
+newTicket("Paul", "Too long wait times", "High")
+newTicket("Zack", "Too short wait times", "Low")
+
